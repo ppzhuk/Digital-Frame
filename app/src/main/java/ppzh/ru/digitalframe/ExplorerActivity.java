7e23ab138ced607ae71d65c68de04a11df812b1b
Zhuk Pavel <ppzhuk@gmail.com>
2016-01-10 16:54:41 +0300

    add login with OAuth

--- app/src/main/java/ppzh/ru/digitalframe/ExplorerActivity.java
+++ app/src/main/java/ppzh/ru/digitalframe/ExplorerActivity.java
@@ -1,17 +1,31 @@
 package ppzh.ru.digitalframe;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.net.URI;
+import java.util.UUID;
 
 public class ExplorerActivity extends AppCompatActivity {
+    private static final String TAG = "ExplorerActivity";
+    private static final String DEVICE_ID = "ru.ppzh.digitalfrane.DEVICE_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explorer);
+        Intent i = getIntent();
+        Uri url = i.getData();
+        if (url != null)
+            Log.i(TAG, url.toString());
     }
 
     @Override
@@ -23,7 +37,41 @@ public class ExplorerActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO: add menu actions
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.menu_login:
+                yandexDiskLogin();
+                return true;
+            // TODO: add menu actions
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void yandexDiskLogin() {
+        String authURL = generateAuthURL();
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(authURL));
+        startActivity(i);
+    }
+
+    private String generateAuthURL() {
+        StringBuilder url =
+                new StringBuilder("https://oauth.yandex.ru/authorize?response_type=token");
+        url.append("&client_id=ec10556277544ff495a5e04c7dd44e0b");
+
+        SharedPreferences sp = this.getPreferences(MODE_PRIVATE);
+        String device_id = sp.getString(DEVICE_ID, null);
+        if (device_id == null) {
+            device_id = UUID.randomUUID().toString();
+            sp.edit().putString(DEVICE_ID, device_id).apply();
+        }
+
+        Log.i(TAG, "device_id - " + device_id);
+
+        url.append("&device_id=");
+        url.append(device_id);
+        url.append("&device_name=");
+        url.append(android.os.Build.MODEL);
+
+        return url.toString();
     }
 }
