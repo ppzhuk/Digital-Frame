0e3c7b6946b9114fe1706f82360a6c34ccd675c2
Zhuk Pavel <ppzhuk@gmail.com>
2016-01-10 04:16:06 +0300

    add options menu

--- app/src/main/java/ppzh/ru/digitalframe/MainActivity.java
+++ app/src/main/java/ppzh/ru/digitalframe/MainActivity.java
@@ -2,6 +2,9 @@ package ppzh.ru.digitalframe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO: add menu actions
+        return super.onOptionsItemSelected(item);
     }
 }
