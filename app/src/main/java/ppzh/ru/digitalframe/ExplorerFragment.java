1e36ce7a4ef1656de64d117541d1b59b30c49fb3
Zhuk Pavel <ppzhuk@gmail.com>
2016-01-11 02:50:47 +0300

    add context menu

--- app/src/main/java/ppzh/ru/digitalframe/ExplorerFragment.java
+++ app/src/main/java/ppzh/ru/digitalframe/ExplorerFragment.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yandex.disk.client.Credentials;
 import com.yandex.disk.client.ListItem;
@@ -22,6 +25,7 @@ public class ExplorerFragment extends ListFragment {
     OnFragmentInteractionListener mListener;
     TransportClient client;
     ItemsList list = new ItemsList();
+    ItemsList imagesList = new ItemsList();
 
     ListView listView;
     TextView emptyListTextView;
@@ -133,6 +137,12 @@ public class ExplorerFragment extends ListFragment {
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        registerForContextMenu(getListView());
+    }
+
+    @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
@@ -145,12 +155,60 @@ public class ExplorerFragment extends ListFragment {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        ListItem item = (ListItem)adapter.getItem(info.position);
+        if (item.isCollection()) {
+            menu.setHeaderTitle(item.getDisplayName());
+            menu.add(Menu.NONE, Menu.NONE, Menu.NONE, "Start slideshow");
+        }
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        return super.onContextItemSelected(item);
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        ListItem listItem = (ListItem)adapter.getItem(info.position);
+        downloadImagesList(listItem.getFullPath());
+        return true;
+    }
+
+    private void downloadImagesList(String fullPath) {
+        new AsyncTask<String, Void, Void>(){
+            @Override
+            protected void onPreExecute() {
+                imagesList.clear();
+                listView.setEnabled(false);
+                listView.setBackgroundColor(getResources().getColor(R.color.colorNotEnabled));
+            }
+
+            @Override
+            protected Void doInBackground(String... params) {
+                try {
+                    client.getList(params[0], imagesList);
+                    imagesList.filterImages();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void s) {
+                if (imagesList.getList().size() == 0) {
+                    listView.setEnabled(true);
+                    listView.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    Toast.makeText(getActivity(), R.string.no_images, Toast.LENGTH_LONG).show();
+                } else {
+                    startSlideshow();
+                }
+
+            }
+
+        }.execute(fullPath);
+    }
+
+    private void startSlideshow() {
+
     }
 }
 
--- app/src/main/java/ppzh/ru/digitalframe/ItemsList.java
+++ app/src/main/java/ppzh/ru/digitalframe/ItemsList.java
@@ -15,7 +15,6 @@ public class ItemsList extends ListParsingHandler {
 
     @Override
     public boolean handleItem(ListItem item) {
-
         if (item.isCollection() || item.getContentType().equals("image/jpeg")) {
             list.add(item);
         }
@@ -30,5 +29,17 @@ public class ItemsList extends ListParsingHandler {
 
     public void clear() {
         list.clear();
+    }
+
+    public List<ListItem> filterImages() {
+        int i = 0;
+        while (i < list.size()) {
+            if (list.get(i).isCollection()) {
+                list.remove(i);
+            } else {
+                ++i;
+            }
+        }
+        return list;
     }
 }
